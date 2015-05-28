0e22fec77a49c9987c5757c5848779f955ba0ceb
kwahome <kevowahome@gmail.com>
2015-05-28 19:55:20 +0300

    Connecting to DB

--- Register.php
+++ Register.php
@@ -7,7 +7,9 @@ $email = $_POST["email"];
 $username = $_POST["username"];
 $password = $_POST["password"];
 
+//SQL Statement
 $query = mysqli_prepare($con, "INSERT INTO users(name, email, username, password) VALUES(?,?,?,?)");
+//To avoid SQL Injection
 mysqli_stmt_bind_param($query, "ssss", $name, $email, $username, $password);
 
 mysqli_close($con);
